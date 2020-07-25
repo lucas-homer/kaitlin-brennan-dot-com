f6f7b9af43e1e2aa8d24aed529ecf83777dc1a07
Lucas Homer <lucashomer@lucass-mbp.home>
2020-07-25 11:45:14 -0700

    create sizes theme property and landingCardContainer size

--- web/src/gatsby-plugin-theme-ui/index.js
+++ web/src/gatsby-plugin-theme-ui/index.js
@@ -34,6 +34,9 @@ export default {
     body: 2,
     heading: 1.125,
   },
+  sizes: {
+    landingCardContainer: 600,
+  },
   fontSizes: [12, 14, 16, 18, 20, 24, 30, 36, 48, 60, 72],
   space: [0, 4, 8, 12, 16, 24, 32, 48, 64, 96, 128, 256, 512],
   breakpoints: ["40em", "56em", "64em"],
@@ -61,6 +64,14 @@ export default {
       "&:hover": {
         cursor: "pointer",
       },
+    },
+  },
+  inputs: {
+    primary: {
+      border: "none",
+      borderRadius: "10px 0 0 10px",
+      bg: "white",
+      padding: 2,
     },
   },
   text: {
