628e477403ebf2e0012cdc78e3d8c103d6f72ace
Lucas Homer <lucashomer@lucass-mbp.home>
2020-07-25 16:13:47 -0700

    setup blog post preview and preview grid for landing page

---
+++ web/src/components/blog-post-preview.js
@@ -0,0 +1,30 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import { Link } from "gatsby"
+import { buildImageObj, getBlogUrl } from "../lib/helpers"
+import { imageUrlFor } from "../lib/image-url"
+import BlockText from "./block-text"
+
+export default function BlogPostPreview(props) {
+  return (
+    <Link to={getBlogUrl(props.publishedAt, props.slug.current)}>
+      <h3>{props.title}</h3>
+      <div>
+        {props.mainImage && props.mainImage.asset && (
+          <img
+            src={imageUrlFor(buildImageObj(props.mainImage))
+              .width(600)
+              .height(Math.floor((9 / 16) * 600))
+              .url()}
+            alt={props.mainImage.alt}
+          />
+        )}
+      </div>
+      {props._rawExcerpt && (
+        <div>
+          <BlockText blocks={props._rawExcerpt} />
+        </div>
+      )}
+    </Link>
+  )
+}
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,75 +1,154 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import GraphQLErrorList from "../components/graphql-error-list"
+import { mapEdgesToNodes, filterOutDocsWithoutSlugs } from "../lib/helpers"
+
 import HumaaanLanding from "../components/humaaan-landing"
 
 import SamplesCard from "../components/samples-landing"
 import NewsletterForm from "../components/newsletter-landing"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <section
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <HumaaanLanding
-        sx={{
-          maxWidth: "300px",
-          minWidth: "150px",
-        }}
+export const query = graphql`
+  query IndexPageQuery {
+    site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
+      title
+      description
+      keywords
+    }
+
+    posts: allSanityPost(
+      limit: 2
+      sort: { fields: [publishedAt], order: DESC }
+    ) {
+      edges {
+        node {
+          id
+          publishedAt
+          mainImage {
+            crop {
+              _key
+              _type
+              top
+              bottom
+              left
+              right
+            }
+            hotspot {
+              _key
+              _type
+              x
+              y
+              height
+              width
+            }
+            asset {
+              _id
+            }
+            alt
+          }
+          title
+          _rawExcerpt
+          slug {
+            current
+          }
+        }
+      }
+    }
+  }
+`
+
+const IndexPage = props => {
+  const { data, errors } = props
+
+  if (errors) {
+    return (
+      <Layout>
+        <GraphQLErrorList errors={errors} />
+      </Layout>
+    )
+  }
+
+  const site = (data || {}).site
+  const postNodes = (data || {}).posts
+    ? mapEdgesToNodes(data.posts).filter(filterOutDocsWithoutSlugs)
+    : []
+
+  if (!site) {
+    throw new Error(
+      'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
+    )
+  }
+
+  return (
+    <Layout>
+      <SEO
+        title={site.title}
+        description={site.description}
+        keywords={site.keywords}
       />
-      <article
+      <section
         sx={{
           display: "flex",
-          alignItems: "baseline",
-          flexWrap: "wrap",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        <aside
+        <HumaaanLanding
           sx={{
-            paddingY: [2, 3, 4],
-            paddingRight: [2, 3, 4],
-            paddingLeft: "0",
-            marginRight: [2, 3, 4],
+            maxWidth: "300px",
+            minWidth: "150px",
+          }}
+        />
+        <article
+          sx={{
+            display: "flex",
+            alignItems: "baseline",
+            flexWrap: "wrap",
           }}
         >
-          <h1
+          <aside
             sx={{
-              marginBottom: "0px",
+              paddingY: [2, 3, 4],
+              paddingRight: [2, 3, 4],
+              paddingLeft: "0",
+              marginRight: [2, 3, 4],
             }}
           >
-            Content Creator
-          </h1>
-          <h2>Clicks queen</h2>
-        </aside>
-        <button
-          type="button"
-          sx={{
-            variant: "buttons.primary",
-            fontSize: 4,
-          }}
-        >
-          <Link
-            to="/contact"
+            <h1
+              sx={{
+                marginBottom: "0px",
+              }}
+            >
+              Content Creator
+            </h1>
+            <h2>Clicks queen</h2>
+          </aside>
+          <button
+            type="button"
             sx={{
-              textDecoration: `none`,
-              color: "white",
+              variant: "buttons.primary",
+              fontSize: 4,
             }}
           >
-            I'll bite
-          </Link>
-        </button>
-      </article>
-    </section>
-    <SamplesCard />
-    <NewsletterForm />
-  </Layout>
-)
+            <Link
+              to="/contact"
+              sx={{
+                textDecoration: `none`,
+                color: "white",
+              }}
+            >
+              I'll bite
+            </Link>
+          </button>
+        </article>
+      </section>
+      <SamplesCard />
+      <NewsletterForm />
+    </Layout>
+  )
+}
 
 export default IndexPage
