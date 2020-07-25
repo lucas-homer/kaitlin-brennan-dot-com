8918a6b3266821dab4b9d2b8791ebab39e574268
Lucas Homer <lucashomer@lucass-mbp.home>
2020-07-24 22:59:02 -0700

    build landing hero

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,4 +1,7 @@
-import React from "react"
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+
+// import React from "react"
 import Layout from "../components/layout"
 import HumaaanLanding from "../components/humaaan-landing"
 import SEO from "../components/seo"
@@ -6,11 +9,54 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <HumaaanLanding />
-    </div>
-    <h1>Content Creation</h1>
-    <h2>it's catchy</h2>
+    <section
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <HumaaanLanding
+        sx={{
+          maxWidth: "300px",
+          minWidth: "150px",
+        }}
+      />
+      <article
+        sx={{
+          display: "flex",
+          alignItems: "baseline",
+          flexWrap: "wrap",
+        }}
+      >
+        <aside
+          sx={{
+            paddingY: [2, 3, 4],
+            paddingRight: [2, 3, 4],
+            paddingLeft: "0",
+            marginRight: [2, 3, 4],
+          }}
+        >
+          <h1
+            sx={{
+              marginBottom: "0px",
+            }}
+          >
+            Content Creator
+          </h1>
+          <h2>Clicks queen</h2>
+        </aside>
+        <button
+          type="button"
+          sx={{
+            variant: "buttons.primary",
+            fontSize: 4,
+          }}
+        >
+          I'll bite
+        </button>
+      </article>
+    </section>
   </Layout>
 )
 
