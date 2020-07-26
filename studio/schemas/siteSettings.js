e810fb0bc4315c418c42271056400a8334529e19
Lucas Homer <lucashomer@lucass-mbp.home>
2020-07-25 21:08:38 -0700

    upgrade sanity

--- studio/schemas/siteSettings.js
+++ studio/schemas/siteSettings.js
@@ -3,7 +3,7 @@ export default {
   title: "Site Settings",
   type: "document",
   // You probably want to uncomment the next line once you've made a siteSettings document in the Studio. This will remove the settings document type from the create-menus.
-  __experimental_actions: ["update", "publish"],
+  // __experimental_actions: ["update", "publish"],
   fields: [
     {
       name: "title",
