d632d560be0b757e42c4ba040df781e7c427c7d7
Lucas Homer <lucashomer@lucass-mbp.home>
2020-08-17 20:52:36 -0700

    basic contact form page

--- web/src/pages/contact.js
+++ web/src/pages/contact.js
@@ -1,12 +1,104 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { navigate } from "gatsby"
 
-const ContactPage = () => (
-  <Layout>
-    <SEO title="Contact" />
-    <h2>Contact</h2>
-  </Layout>
-)
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
 
-export default ContactPage
+export default function ContactPage() {
+  const [state, setState] = React.useState({})
+
+  const handleChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error))
+  }
+
+  return (
+    <Layout>
+      <SEO title="Contact" />
+      <h1>Contact</h1>
+      <form
+        name="contact"
+        method="post"
+        action="/thanks/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
+      >
+        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+        <input
+          type="hidden"
+          name="form-name"
+          value="contact"
+          aria-label="ignore this input"
+        />
+        <p hidden>
+          <label>
+            Don’t fill this out:{" "}
+            <input
+              name="bot-field"
+              onChange={handleChange}
+              aria-label="don't fill this out"
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your name:
+            <br />
+            <input
+              type="text"
+              name="name"
+              onChange={handleChange}
+              aria-label="Your name"
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your email:
+            <br />
+            <input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              aria-label="Your email"
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message:
+            <br />
+            <textarea
+              name="message"
+              onChange={handleChange}
+              aria-label="message"
+            />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </Layout>
+  )
+}
---
+++ web/src/pages/thanks.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default function Thanks() {
+  return (
+    <Layout>
+      <SEO title="thanks" />
+      <div>thanks! can't wait to chat.</div>
+    </Layout>
+  )
+}
