0e3f8346a5294364108df1dceced91e8b1699128
Lucas Homer <lucashomer@lucass-mbp.home>
2020-08-23 16:32:34 -0700

    create skeleton samples page and write query for workSamples

---
+++ web/src/components/work-samples.js
@@ -0,0 +1,6 @@
+import React from "react"
+
+export default function WorkSamples({ nodes }) {
+  console.log("nodes", nodes)
+  return <div>Work Samples Explorer</div>
+}
--- web/src/pages/samples.js
+++ web/src/pages/samples.js
@@ -1,12 +1,105 @@
-import React from "react"
+/** @jsx jsx */
+import { jsx, Styled } from "theme-ui"
+import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
+import GraphQLErrorList from "../components/graphql-error-list"
 import SEO from "../components/seo"
+import WorkSamples from "../components/work-samples"
+import { mapEdgesToNodes } from "../lib/helpers"
 
-const SamplesPage = () => (
-  <Layout>
-    <SEO title="Samples" />
-    <h2>Samples</h2>
-  </Layout>
-)
+export const query = graphql`
+  query SamplesPageQuery {
+    workSamples: allSanityWorkSample {
+      edges {
+        node {
+          id
+          title
+          sampleType {
+            title
+          }
+          pdfUpload {
+            asset {
+              id
+              title
+              description
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+function SamplesPage({ data, errors }) {
+  if (errors) {
+    return (
+      <Layout>
+        <GraphQLErrorList errors={errors} />
+      </Layout>
+    )
+  }
+
+  const workSampleNodes = (data || {}).workSamples
+    ? mapEdgesToNodes(data.workSamples)
+    : []
+
+  return (
+    <div
+      sx={{
+        bg: "backgroundGrey",
+      }}
+    >
+      <Layout>
+        <SEO title="Samples" />
+        <section
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            padding: 4,
+            maxWidth: "400px",
+            margin: "2rem auto",
+          }}
+        >
+          <Styled.h3
+            sx={{
+              textAlign: "center",
+            }}
+          >
+            Work Samples
+          </Styled.h3>
+          <Styled.h6
+            sx={{
+              textAlign: "center",
+            }}
+          >
+            Interested in more samples, references, or want to chat about a
+            potential project?
+          </Styled.h6>
+          <button
+            sx={{
+              variant: "buttons.secondary",
+              maxWidth: "200px",
+              margin: "0 auto",
+            }}
+          >
+            <Link
+              to="/contact"
+              sx={{
+                textDecoration: `none`,
+                color: "primary",
+              }}
+            >
+              Reach out!
+            </Link>
+          </button>
+        </section>
+
+        <WorkSamples nodes={workSampleNodes} />
+      </Layout>
+    </div>
+  )
+}
 
 export default SamplesPage
