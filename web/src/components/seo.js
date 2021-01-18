08c5ab1e8e55b6e8e3d0d54862a1c7286cc93175
lucas homer <lucas.homer@gmail.com>
2021-01-17 17:20:33 -0800

    trying this again

--- web/src/components/seo.js
+++ web/src/components/seo.js
@@ -29,12 +29,13 @@ function SEO({ description, lang, meta, title, mainImage }) {
 
   const metaDescription = description || site.siteMetadata.description
 
-  const imageUrl =
-    imageUrlFor(buildImageObj(mainImage))
-      .width(1200)
-      .height(Math.floor((9 / 16) * 1200))
-      .fit("crop")
-      .url() || ""
+  const imageUrl = mainImage?.asset
+    ? imageUrlFor(buildImageObj(mainImage))
+        .width(1200)
+        .height(Math.floor((9 / 16) * 1200))
+        .fit("crop")
+        .url()
+    : ""
 
   return (
     <Helmet
