69a4098003d7c69cca94462b75f28404fe416c23
lucas homer <lucas.homer@gmail.com>
2021-02-01 16:47:33 -0800

    polish the newsletter lambda and landing page version of the form

--- web/functions/submission-created/submission-created.js
+++ web/functions/submission-created/submission-created.js
@@ -6,20 +6,50 @@
 // // details in https://css-tricks.com/using-netlify-forms-and-netlify-functions-to-build-an-email-sign-up-widget
 const fetch = require("node-fetch")
 const { EMAIL_TOKEN } = process.env
+
 exports.handler = async event => {
+  console.log(JSON.parse(event.body)) // todo -- delete this once you're one developing, we don't need this in the netlify logs
+
   const email = JSON.parse(event.body).payload.email
-  console.log(`Recieved a submission: ${email}`)
-  return fetch("https://api.buttondown.email/v1/subscribers", {
-    method: "POST",
-    headers: {
-      Authorization: `Token ${EMAIL_TOKEN}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ email }),
-  })
-    .then(response => response.json())
-    .then(() => {
-      console.log(`Submitted to Buttondown`)
-    })
-    .catch(error => ({ statusCode: 422, body: String(error) }))
+  const formName = JSON.parse(event.body).payload.data["form-name"]
+
+  if (formName === "newsletter") {
+    console.log(`Recieved a Newsletter submission: ${email}`)
+    try {
+      const response = await fetch(
+        "https://api.buttondown.email/v1/subscribers",
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Token ${EMAIL_TOKEN}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email }),
+        }
+      )
+      const data = await response.json()
+
+      if (response.status === 400) {
+        return {
+          statusCode: 400,
+          body: JSON.stringify({
+            message: "Looks like that address already subscribed.",
+          }),
+        }
+      }
+
+      console.log(`New subscriber submitted to Buttondown - ${data.email}`)
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Success!" }),
+      }
+    } catch (error) {
+      console.log("error", error)
+      throw new Error("Oh no! There was an error")
+    }
+  }
+
+  if (formName === "contact") {
+    console.log("You need to implement this!")
+  }
 }
--- web/src/components/newsletter-landing.js
+++ web/src/components/newsletter-landing.js
@@ -6,7 +6,7 @@ import { encode } from "../lib/helpers"
 export default function NewsletterLanding() {
   const [state, setState] = React.useState({})
   const [isLoading, setIsLoading] = React.useState(false)
-  const [isError, setIsError] = React.useState(false)
+  const [error, setError] = React.useState("")
   const [showSuccessEmoji, setShowSuccessEmoji] = React.useState(false)
   const handleChange = e => {
     setState({ ...state, [e.target.name]: e.target.value })
@@ -15,11 +15,13 @@ export default function NewsletterLanding() {
   const handleSubmit = async e => {
     e.preventDefault()
     setIsLoading(true)
+    setShowSuccessEmoji(false)
+    setError("")
 
     const form = e.target
 
     try {
-      await fetch("/", {
+      const response = await fetch("/", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: encode({
@@ -28,129 +30,134 @@ export default function NewsletterLanding() {
         }),
       })
 
-      form.reset()
+      const body = await response.json()
 
-      setIsError(false)
+      if (response.status === 400) {
+        setError(body.message)
+        return
+      }
+
+      form.reset()
       setShowSuccessEmoji(true)
     } catch (error) {
-      setShowSuccessEmoji(false)
-      setIsError(true)
+      setError("Oh no! There was an error.")
     } finally {
       setIsLoading(false)
     }
   }
 
   return (
-    <section
-      sx={{
-        borderRadius: "primary",
-        bg: "backgroundGrey",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        padding: 4,
-        maxWidth: "landingCardContainer",
-        margin: "2rem auto",
-        filter: "drop-shadow(16px 16px 20px #cecece)",
-      }}
-    >
-      <div
-        sx={{
-          margin: "0 auto",
-        }}
-      >
-        <Styled.h3
+    <>
+      {showSuccessEmoji ? (
+        <h4
           sx={{
+            color: "purp",
+            textAlign: "center",
             marginBottom: ".5rem",
           }}
         >
-          I offer delivery{" "}
-          <span role="img" aria-label="painting fingernails">
-            💅
+          {" "}
+          <span role="img" aria-label="purple heart">
+            💜
+          </span>{" "}
+          Thanks! I sent an email to confirm.{" "}
+          <span role="img" aria-label="purple heart">
+            💜
           </span>
-        </Styled.h3>
-        <Styled.h5>Homemade thoughts to your inbox</Styled.h5>
-        {showSuccessEmoji ? (
-          <h4
-            sx={{
-              color: "purp",
-              textAlign: "center",
-              marginBottom: ".5rem",
-            }}
-          >
-            {" "}
-            <span role="img" aria-label="purple heart">
-              💜
-            </span>{" "}
-            Thanks! I sent an email to confirm.{" "}
-            <span role="img" aria-label="purple heart">
-              💜
-            </span>
-          </h4>
-        ) : null}
-        {isError ? (
-          <h4
-            sx={{
-              color: "highlight",
-              textAlign: "center",
-              marginBottom: ".5rem",
-            }}
-          >
-            Oh no! There was an error. Try again?
-          </h4>
-        ) : null}
-      </div>
-
-      <form
-        name="newsletter"
-        method="POST"
-        action="/"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        onSubmit={handleSubmit}
-        sx={{
-          margin: "0 auto",
-        }}
-      >
-        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-        <input
-          type="hidden"
-          name="form-name"
-          value="contact"
-          aria-label="ignore this input"
-        />
-        <p hidden>
-          <label>
-            Don’t fill this out:{" "}
-            <input
-              name="bot-field"
-              onChange={handleChange}
-              aria-label="don't fill this out"
-            />
-          </label>
-        </p>
-
-        <input
-          onChange={handleChange}
-          aria-label="Email"
-          type="email"
-          name="email"
-          placeholder="Email"
+        </h4>
+      ) : error ? (
+        <h4
           sx={{
-            variant: "inputs.primary",
-          }}
-        />
-        <button
-          type="submit"
-          sx={{
-            variant: "buttons.secondary",
-            fontSize: 4,
-            marginLeft: "-8px",
+            color: "highlight",
+            textAlign: "center",
+            marginBottom: ".5rem",
           }}
         >
-          {isLoading ? "   ...   " : "Subscribe"}
-        </button>
-      </form>
-    </section>
+          {error}
+        </h4>
+      ) : null}
+      <section
+        sx={{
+          borderRadius: "primary",
+          bg: "backgroundGrey",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          padding: 4,
+          maxWidth: "landingCardContainer",
+          margin: "2rem auto",
+          filter: "drop-shadow(16px 16px 20px #cecece)",
+        }}
+      >
+        <div
+          sx={{
+            margin: "0 auto",
+          }}
+        >
+          <Styled.h3
+            sx={{
+              marginBottom: ".5rem",
+            }}
+          >
+            I offer delivery{" "}
+            <span role="img" aria-label="painting fingernails">
+              💅
+            </span>
+          </Styled.h3>
+          <Styled.h5>Homemade thoughts to your inbox</Styled.h5>
+        </div>
+
+        <form
+          name="newsletter"
+          method="POST"
+          action="/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={handleSubmit}
+          sx={{
+            margin: "0 auto",
+          }}
+        >
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input
+            type="hidden"
+            name="form-name"
+            value="contact"
+            aria-label="ignore this input"
+          />
+          <p hidden>
+            <label>
+              Don’t fill this out:{" "}
+              <input
+                name="bot-field"
+                onChange={handleChange}
+                aria-label="don't fill this out"
+              />
+            </label>
+          </p>
+
+          <input
+            onChange={handleChange}
+            aria-label="Email"
+            type="email"
+            name="email"
+            placeholder="Email"
+            sx={{
+              variant: "inputs.primary",
+            }}
+          />
+          <button
+            type="submit"
+            sx={{
+              variant: "buttons.secondary",
+              fontSize: 4,
+              marginLeft: "-8px",
+            }}
+          >
+            {isLoading ? "Loading..." : "Subscribe"}
+          </button>
+        </form>
+      </section>
+    </>
   )
 }
