b0df1d610920cc5087c4e5d142c058797c946704
lucas homer <lucas.homer@gmail.com>
2021-02-24 19:38:39 -0800

    add a gif to the contact page because why not

--- web/src/pages/contact.js
+++ web/src/pages/contact.js
@@ -2,7 +2,7 @@
 import { jsx } from "theme-ui"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import SittingHuman from "../components/sitting-human"
+// import SittingHuman from "../components/sitting-human"
 import ContactForm from "../components/contact-form"
 
 export default function ContactPage() {
@@ -23,14 +23,29 @@ export default function ContactPage() {
             padding: "2rem",
           }}
         >
-          <SittingHuman
+          <div
             sx={{
+              width: "100%",
+              height: "0",
+              paddingBottom: "55%",
+              position: "relative",
               maxWidth: "35%",
               "@media screen and (max-width: 650px)": {
                 display: "none",
               },
             }}
-          />
+          >
+            <iframe
+              src="https://giphy.com/embed/OCu7zWojqFA1W"
+              width="100%"
+              height="100%"
+              style={{ position: "absolute" }}
+              frameBorder="0"
+              class="giphy-embed"
+              allowFullScreen
+              title="Hold On To Your Butts Jurassic Park gif"
+            ></iframe>
+          </div>
           <ContactForm />
         </section>
       </Layout>
