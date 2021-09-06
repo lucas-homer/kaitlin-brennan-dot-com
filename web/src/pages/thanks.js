0aa6ff090765bc5674c54ade53346335a31e0897
lucas homer <lucas.homer@gmail.com>
2021-09-06 13:47:47 -0700

    remove unnecessary outer div

--- web/src/pages/thanks.js
+++ web/src/pages/thanks.js
@@ -7,71 +7,63 @@ import PottedPlant from "../components/potted-plant"
 
 export default function Thanks() {
   return (
-    <div
-      sx={
-        {
-          // bg: "backgroundGrey",
-        }
-      }
-    >
-      <Layout>
-        <SEO title="thanks" />
-        <section
+    <Layout>
+      <SEO title="thanks" />
+      <section
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 4,
+          maxWidth: "landingCardContainer",
+          margin: "4rem auto 0",
+        }}
+      >
+        <div sx={{ paddingRight: [7], paddingLeft: [8], paddingBottom: [7] }}>
+          <PottedPlant />
+        </div>
+        <Styled.h4
           sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            padding: 4,
-            maxWidth: "landingCardContainer",
-            margin: "4rem auto 0",
+            textAlign: "center",
+            marginBottom: [1],
           }}
         >
-          <div sx={{ paddingRight: [7], paddingLeft: [8], paddingBottom: [7] }}>
-            <PottedPlant />
-          </div>
-          <Styled.h4
+          Thank you so much!{" "}
+        </Styled.h4>
+        <Styled.h5
+          sx={{
+            textAlign: "center",
+            fontFamily: "body",
+            fontWeight: "body",
+          }}
+        >
+          <span role="img" aria-label="praying hands">
+            🙏
+          </span>{" "}
+          I can't wait to connect{" "}
+          <span role="img" aria-label="praying hands">
+            🙏
+          </span>{" "}
+        </Styled.h5>
+        <button
+          sx={{
+            variant: "buttons.secondary",
+            maxWidth: "200px",
+            margin: "12px auto ",
+          }}
+        >
+          <Link
+            to="/"
             sx={{
-              textAlign: "center",
-              marginBottom: [1],
+              textDecoration: `none`,
+              color: "primary",
             }}
           >
-            Thank you so much!{" "}
-          </Styled.h4>
-          <Styled.h5
-            sx={{
-              textAlign: "center",
-              fontFamily: "body",
-              fontWeight: "body",
-            }}
-          >
-            <span role="img" aria-label="praying hands">
-              🙏
-            </span>{" "}
-            I can't wait to connect{" "}
-            <span role="img" aria-label="praying hands">
-              🙏
-            </span>{" "}
-          </Styled.h5>
-          <button
-            sx={{
-              variant: "buttons.secondary",
-              maxWidth: "200px",
-              margin: "12px auto ",
-            }}
-          >
-            <Link
-              to="/"
-              sx={{
-                textDecoration: `none`,
-                color: "primary",
-              }}
-            >
-              Back to Home page
-            </Link>
-          </button>
-        </section>
-      </Layout>
-    </div>
+            Back to Home page
+          </Link>
+        </button>
+      </section>
+    </Layout>
   )
 }
